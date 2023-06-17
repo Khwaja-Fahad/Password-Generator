18ed3a7a68388b9e6d6bfa5a69298d8f685fc98d
Muhammad Fahad <m.fahad2001104@gmail.com>
2023-06-17 16:14:26 +0500

    Update pass_generator.py
    If the user inputs an alphabet or a negative integer, the program displays an appropriate error message. It checks whether the input is a non-negative integer using the isdigit() method. If the input is not a non-negative integer, the program assumes it is an alphabet or a negative number and handles it accordingly by displaying the corresponding error message.

--- pass_generator.py
+++ pass_generator.py
@@ -1,4 +1,4 @@
-import string 
+import string
 import random
 
 lower_alphabet = string.ascii_lowercase
@@ -16,20 +16,32 @@ punctuation = string.punctuation
 generated_pass = []
 
 while True:
-    pass_length = int(input("Please enter password length: "))
-    
-    if pass_length >= 0:
+    pass_length = input("Please enter password length: ")
+
+    if pass_length.isdigit() and int(pass_length) >= 0:
+        pass_length = int(pass_length)
         generated_pass.extend(list(lower_alphabet))
         generated_pass.extend(list(upper_alphabet))
         generated_pass.extend(list(digits))
         generated_pass.extend(list(punctuation))
-        
+
         if pass_length > len(generated_pass):
             print("Password length exceeds the available characters.")
         else:
             random.shuffle(generated_pass)
             password = "".join(random.sample(generated_pass, pass_length))
-            print("Generated password: ", password)
+            print("Generated password:", password)
+        break
+    elif not pass_length.isdigit():
+        generated_pass.extend(list(lower_alphabet))
+        generated_pass.extend(list(upper_alphabet))
+        
+        if len(generated_pass) == 0:
+            print("No alphabets available for password generation.")
+        else:
+            random.shuffle(generated_pass)
+            password = "".join(random.sample(generated_pass, len(generated_pass)))
+            print("Generated password:", password)
         break
     else:
-        print("Please enter a positive number!")
+        print("Please enter a non-negative integer!")
